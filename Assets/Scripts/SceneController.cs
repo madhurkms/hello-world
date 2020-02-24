6dd3650f60b883bf5b4c8eee8b83cd5cb65a3f58
madhuAntiz <madhu@antiztech.com>
2020-02-24 12:13:25 +0530

    Feature1-buttons

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(String SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
